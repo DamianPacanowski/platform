8daa8afb4c564bb677f0c51197bc05d058d55525
PACANOSIU <60273697+DamianPacanowski@users.noreply.github.com>
2023-02-23 11:39:28 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,81 @@
+<?php
+	if($_SERVER['REQUEST_URI']=='/')
+	{
+		$dir='../'.$_SERVER['SERVER_NAME'];
+	}
+	else
+	{
+		$dir='..'.$_SERVER['REQUEST_URI'];
+	}
+	if(is_dir($dir))
+	{
+		$scandir=scandir($dir);
+		foreach($scandir as $_dir)
+		{
+			if(is_dir($dir.'/'.$_dir))
+			{
+				$array_hlinks[]=array($_dir);
+			}
+		}
+	}
+	else
+	{
+		$explode_uri=explode('/',$_SERVER['REQUEST_URI']);		
+		foreach($explode_uri as $uri)
+		{
+			if($uri!='')
+			{
+				$dir='../'.$uri;
+			}
+		}		
+		if(is_dir($dir))
+		{
+			$scandir=scandir($dir);
+			foreach($scandir as $_dir)
+			{
+				if(is_dir($dir.'/'.$_dir))
+				{
+					$array_hlinks[]=array($_dir);
+				}
+			}
+		}
+	}		
+?>
+<header>
+	<a href="/">
+		<div class="icon">
+			<div>
+				<?php echo str_replace('.pl','',$_SERVER['SERVER_NAME']);?>
+			</div>
+		</div>
+	</a>	
+	<a href="<?php echo$_SERVER['REQUEST_URI'];?>">
+		<div class="menu">
+			<div>
+				<?php echo strtoupper(str_replace('/',' ',$_SERVER['REQUEST_URI']));?>
+			</div>
+		</div>
+	</a>
+<?php
+	foreach($array_hlinks as $no =>$null)
+	{
+		foreach($array_hlinks[$no] as $hlink)
+		{
+			if(($hlink!='.')&&($hlink!='..'))
+			{
+				echo
+					'<a href="'.$_SERVER['REQUEST_URI'].''.$hlink.'">
+						<div class="menu">
+							<div>
+								'.$hlink.'
+							</div>
+						</div>
+					</a>'
+				;
+			}
+		}
+	}
+?>
+</header>
+
+
